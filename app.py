6c457cf6fc7ef86c74de6ad249ab6556400ef63f
Aditi Mondal <mondaladiti75@gmail.com>
2024-04-22 00:50:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -133,6 +133,7 @@ def direct_feed():
                 }
                 .rectangular-box p {
                     margin: 0;
+                    font-size:22px;
                 }
             </style>
         """
